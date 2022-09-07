3185a8220c3e617689ca172db11297786eee240f
ishveer1026 <107894915+ishveer1026@users.noreply.github.com>
2022-09-07 16:31:45 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-
+var array=[];
+function submit(){
+for(var i=1; i<5; i++){
+  var name=document.getElementById("name_of_the_student_"+i).value;
+  array.push(name);
+  }
+  var length=array.length;
+  var display=[];
+  for(var j=0; j<length; j++){
+    display.push("<h4> name-"+ array[j]+"</h4>");
+    console.log(display);
+  }
+  document.getElementById("display_name_with_commas").innerHTML=display;
+  var remove_commas=display.join(" ");
+    document.getElementById("display_name_without_commas").innerHTML=remove_commas;
+}
